af2ef55c2ec5fca7610d9e21a4bb6448658cc6b3
Carlos Avila <35896114-cavila12@users.noreply.replit.com>
2024-09-15 16:31:36 +0000

    fin?

--- main.cs
+++ main.cs
@@ -1,7 +1,12 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+  static void Main (string[] args) {
+    const double CentiPerInch = 2.54;
+    double inches;
+    Console.Write("Enter a number of inches: ");
+    inches = Convert.ToDouble(Console.ReadLine());
+    double centimeters = inches * CentiPerInch;
+    Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters);
   }
 }
